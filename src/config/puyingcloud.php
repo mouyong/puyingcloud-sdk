d167100c80295133b8da5c337b10dfcedaa0f074
牟勇 <my24251325@gmail.com>
2019-07-31 11:57:03 +0800

    修改配置文件默认配置

--- src/config/puyingcloud.php
+++ src/config/puyingcloud.php
@@ -10,14 +10,14 @@
 
 return [
     'debug' => true, // 必须有，不然 foundation 72 行会报 Notice 未定义索引 debug 错误
-    'phone' => 'your-phone-number',
-    'password' => 'your-password',
+    'phone' => env('PUYINGCLOUD_PHONE', 'your-phone-number'),
+    'password' => env('PUYINGCLOUD_PASSWORD', 'your-password'),
 
     'log' => [
-        'file' => __DIR__.'/runtime.log',
+        'file' => storage_path('logs/puyingcloud.log'),
         'level' => 'debug',
         'permission' => 0777,
     ],
 
-    'cache' => new \Doctrine\Common\Cache\FilesystemCache(__DIR__.'../cache/'),
+    'cache' => new \Doctrine\Common\Cache\FilesystemCache(storage_path('app/framework/cache')),
 ];
