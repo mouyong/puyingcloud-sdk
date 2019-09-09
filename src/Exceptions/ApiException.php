b70b76f28153e8351467ae3d853d308dd9001515
牟勇 <my242551325@gmail.com>
2019-09-09 22:05:45 +0800

    添加英文消息转中文

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -11,7 +11,31 @@
 namespace Yan\PuyingCloudSdk\Exceptions;
 
 use RuntimeException;
+use Throwable;
 
 class ApiException extends RuntimeException
 {
+    const PRINTER_ALREADY_SUBSCRIBED = 40013007;
+    const SN_UNEXISTS = 40013002;
+
+    const EXCEPTION_MESSAGE_MAP = [
+        self::PRINTER_ALREADY_SUBSCRIBED => '小票机已绑定，请先解绑',
+        self::SN_UNEXISTS => '小票机不存在',
+    ];
+
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        $message = $this->resolveMessage($code, $message);
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function resolveMessage($code, $message = '')
+    {
+        if (array_key_exists($code, self::EXCEPTION_MESSAGE_MAP)) {
+            return self::EXCEPTION_MESSAGE_MAP[$code];
+        }
+
+        return $message;
+    }
 }
