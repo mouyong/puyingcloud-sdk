e0a11cd441779a01b02755cfa435fe6daab0328f
mouyong <mouyong@users.noreply.github.com>
2019-09-09 22:06:09 +0800

    Apply fixes from StyleCI (#3)

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -16,6 +16,7 @@ use Throwable;
 class ApiException extends RuntimeException
 {
     const PRINTER_ALREADY_SUBSCRIBED = 40013007;
+
     const SN_UNEXISTS = 40013002;
 
     const EXCEPTION_MESSAGE_MAP = [
@@ -23,7 +24,7 @@ class ApiException extends RuntimeException
         self::SN_UNEXISTS => '小票机不存在',
     ];
 
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
     {
         $message = $this->resolveMessage($code, $message);
 
