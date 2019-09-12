4a95dc332e1d0f90838e433404d92b4e0d7c1485
牟勇 <my242551325@gmail.com>
2019-09-12 12:16:21 +0800

    更新错误信息

--- src/Exceptions/ApiException.php
+++ src/Exceptions/ApiException.php
@@ -20,7 +20,7 @@ class ApiException extends RuntimeException
     const SN_UNEXISTS = 40013002;
 
     const EXCEPTION_MESSAGE_MAP = [
-        self::REQUEST_BODY_ERROR => '服务器错误，请稍后重试',
+        self::REQUEST_BODY_ERROR => '请检查输入信息是否正确',
         self::PRINTER_ALREADY_SUBSCRIBED => '小票机已绑定，请先解绑',
         self::SN_UNEXISTS => '小票机不存在',
     ];
